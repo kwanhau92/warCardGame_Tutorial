85b72025fb22e9e13d269f776194984167a2c178
Ming kwan Hau <kwanhau92@MingdeAir.mshome.net>
2020-12-04 16:49:37 -0500

    Added function onto ViewController.swift

--- warCardGame/warCardGame/ViewController.swift
+++ warCardGame/warCardGame/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+
+    @IBOutlet weak var playerScore: UILabel!
+    
+    @IBOutlet weak var cpuScore: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftNumber = Int.random(in: 2 ... 14)
+        let rightNumber = Int.random(in: 2 ... 14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber{
+            leftScore += 1
+            playerScore.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+            rightScore += 1
+            cpuScore.text = String(rightScore)
+        }
+        else{
+            print("here")
+        }
+        return
+    }
+    
 }
 
